37fe15779649aade9035ea6568127cb4406081b2
mnekx <karimnembac@gmail.com>
2022-09-02 15:18:54 +0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,9 @@
 import pytest
+from brownie import Raffle
+
 from scripts.deploy_lottery import deploy_lottery
 
 @pytest.fixture()
 def lottery_contract():
-    return deploy_lottery()
+    raffle_contract = Raffle[-1] if len(Raffle) > 0 else deploy_lottery()
+    return raffle_contract
